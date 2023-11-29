8dda0a9eaec473fd2c31e3f65fba7e5c964b3ae3
NyPadilha <763npp@gmail.com>
2023-11-29 20:33:15 -0300

    <test>: <encrypt_message>

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("teste", "teste")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 1)
+
+    assert encrypt_message("jaguara", 0) == "araugaj"
+    assert encrypt_message("jaguara", 1) == "j_arauga"
