7147942e08ccffc559549e6e8f6cf072f6a17685
NyPadilha <763npp@gmail.com>
2023-11-29 17:55:00 -0300

    <feat>: <students at the same hour-search>

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # sum(1 for start, end in permanence_period if start <= target_time <= end)
+    if not isinstance(target_time, int):
+        return None
+
+    count = 0
+
+    for period in permanence_period:
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
+            return None
+
+        if period[0] <= target_time <= period[1]:
+            count += 1
+
+    return count
